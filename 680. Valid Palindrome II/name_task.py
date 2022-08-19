30b4cf9184d017c9c7cb497427543f7b106513ad
vadim-shalapugin <vadimsalapugin@gmail.com>
2022-08-19 20:10:01 +0300

    2validpalim

---
+++ 680. Valid Palindrome II/name_task.py
@@ -0,0 +1,7 @@
+"""Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+"""
