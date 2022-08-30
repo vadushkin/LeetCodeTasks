f2c81bfb3f27b1844492d6cc38227d75addc23c7
vadim-shalapugin <vadimsalapugin@gmail.com>
2022-08-30 17:50:24 +0300

    rotate image

---
+++ 48. Rotate Image/v2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rotate(self, matrix: list[list[int]]) -> None:
+        matrix.reverse()
+        for i in range(len(matrix)):
+            for j in range(i + 1, len(matrix[i])):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+def main():
+    s = Solution()
+    print(s.rotate([[1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]]
+                   ))
+
+
+if __name__ == "__main__":
+    main()
+
+"""Tests:
+1. Runtime: 36 ms, faster than 93.87% of Python3 online submissions for Rotate Image.
+Memory Usage: 13.9 MB, less than 29.99% of Python3 online submissions for Rotate Image.
+
+2. Runtime: 60 ms, faster than 39.22% of Python3 online submissions for Rotate Image.
+Memory Usage: 13.9 MB, less than 74.32% of Python3 online submissions for Rotate Image.
+"""
