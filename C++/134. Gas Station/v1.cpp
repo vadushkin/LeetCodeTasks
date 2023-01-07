28ba2eeacf23f46cbe370d621c52e87c783e88f0
vaduska <vadimsalapugin@gmail.com>
2023-01-07 18:28:22 +0500

    134

--- C++/134. Gas Station/v1.cpp
+++ C++/134. Gas Station/v1.cpp
@@ -1,3 +1,46 @@
-//
-// Created by vadim on 07.01.23.
-//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        int total_gas = 0, total_cost = 0;
+        int curr_gas = 0, starting_point = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+
+            total_gas += gas[i];
+            total_cost += cost[i];
+            curr_gas += gas[i] - cost[i];
+
+            if (curr_gas < 0) {
+                starting_point = i + 1;
+                curr_gas = 0;
+            }
+        }
+        return (total_gas < total_cost) ? -1 : starting_point;
+    }
+};
+
+
+int main() {
+
+    Solution s;
+
+    vector<int> gas = {1, 2, 3, 4, 5};
+    vector<int> cost = {3, 4, 5, 1, 2};
+
+    cout << s.canCompleteCircuit(gas, cost) << endl;
+
+    return 0;
+}
+
+/* Tests:
+1. Runtime 91 ms Beats 80.36%
+   Memory 69.4 MB Beats 55.78%
+
+2. Runtime 98 ms Beats 68.85%
+   Memory 69.5 MB Beats 12.90%
+*/
--- C++/134. Gas Station/v2.cpp
+++ C++/134. Gas Station/v2.cpp
@@ -1,3 +1,48 @@
-//
-// Created by vadim on 07.01.23.
-//
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        int sum_gas = accumulate(begin(gas), end(gas), 0, plus<int>());
+        int sum_cost = accumulate(begin(cost), end(cost), 0, plus<int>());
+
+        if (sum_gas < sum_cost) return -1;
+
+        int tank = 0, index = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+            tank += gas[i] - cost[i];
+
+            if (tank < 0) {
+                tank = 0;
+                index = i + 1;
+            }
+        }
+        return index;
+    }
+};
+
+
+int main() {
+
+    Solution s;
+
+    vector<int> gas = {1, 2, 3, 4, 5};
+    vector<int> cost = {3, 4, 5, 1, 2};
+
+    cout << s.canCompleteCircuit(gas, cost) << endl;
+
+    return 0;
+}
+
+/* Tests:
+1. Runtime 69 ms Beats 99.52%
+   Memory 69.5 MB Beats 55.78%
+
+2. Runtime 88 ms Beats 84.98%
+   Memory 69.5 MB Beats 12.90%
+*/
