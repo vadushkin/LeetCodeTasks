6593a3fff68a3b9cfe174216ae63cec40713f4a0
Vadim <vadimsalapugin@gmail.com>
2023-02-15 16:26:31 +0500

    917

---
+++ Python/900-1000/917. Reverse Only Letters/v1_mobile.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverseOnlyLetters(self, S: str) -> str:
+        S, i, j = list(S), 0, len(S) - 1
+
+        while i < j:
+
+            if not S[i].isalpha():
+
+                i += 1
+
+            elif not S[j].isalpha():
+
+                j -= 1
+
+            else:
+
+                S[i], S[j] = S[j], S[i]
+
+                i, j = i + 1, j - 1
+
+        return "".join(S)
