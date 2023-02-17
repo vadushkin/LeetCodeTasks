735ba4827d699d623c852c081643e8e8c9310919
Vadim <vadimsalapugin@gmail.com>
2023-02-17 16:29:59 +0500

    56

---
+++ Python/1-100/56. Merge Intervals/v1_mobile.py
@@ -0,0 +1,9 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        out = []
+        for i in sorted(intervals, key=lambda i: i[0]):
+            if out and i[0] <= out[-1][1]:
+                out[-1][1] = max(out[-1][1], i[1])
+            else:
+                out += [i]
+        return out
