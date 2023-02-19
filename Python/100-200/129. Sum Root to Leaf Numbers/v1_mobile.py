6da6bd40397597b85edd1a16c336bc9709dd1e79
Vadim <vadimsalapugin@gmail.com>
2023-02-19 15:01:26 +0500

    129

---
+++ Python/100-200/129. Sum Root to Leaf Numbers/v1_mobile.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+                
+        self.res = 0
+
+        self.dfs(root, 0)
+
+        return self.res
+
+    
+
+    def dfs(self, root, path):
+
+        if root:
+
+            if not root.left and not root.right:
+
+                path = path*10 + root.val
+
+                self.res += path
+
+            self.dfs(root.left, path*10+root.val)
+
+            self.dfs(root.right, path*10+root.val)
