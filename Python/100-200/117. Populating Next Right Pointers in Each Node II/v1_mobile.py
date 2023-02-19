b9787b696ff671539d094c0355a489504b6104a5
Vadim <vadimsalapugin@gmail.com>
2023-02-19 15:00:10 +0500

    117

---
+++ Python/100-200/117. Populating Next Right Pointers in Each Node II/v1_mobile.py
@@ -0,0 +1,29 @@
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        node = root
+
+        while node:
+
+            curr = dummy = Node(0)
+
+            while node:
+
+                if node.left:
+
+                    curr.next = node.left
+
+                    curr = curr.next
+
+                if node.right:
+
+                    curr.next = node.right
+
+                    curr = curr.next
+
+                node = node.next
+
+            node = dummy.next
+
+               
+
+        return root
