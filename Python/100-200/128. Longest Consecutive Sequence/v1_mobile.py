a5ca840f3fb8d8320025eb2566e9786e86f088cc
Vadim <vadimsalapugin@gmail.com>
2023-02-19 15:00:48 +0500

    128

---
+++ Python/100-200/128. Longest Consecutive Sequence/v1_mobile.py
@@ -0,0 +1,36 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest, s = 0, set(nums)
+
+ 
+        for num in nums:
+
+ 
+            cur_longest, j = 1, 1
+
+ 
+            while num - j in s: 
+
+  
+                s.remove(num - j)
+
+  
+                cur_longest, j = cur_longest + 1, j + 1
+
+ 
+            j = 1
+
+ 
+            while num + j in s: 
+
+  
+                s.remove(num + j)
+
+  
+                cur_longest, j = cur_longest + 1, j + 1
+
+  
+            longest = max(longest, cur_longest)
+
+ 
+        return longest
