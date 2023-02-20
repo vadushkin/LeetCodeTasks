e82675968de462b0849ec946639bf253157d3e86
Vadim <vadimsalapugin@gmail.com>
2023-02-20 16:03:23 +0500

    143

---
+++ Python/100-200/143. Reorder List/v1_mobile.py
@@ -0,0 +1,46 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head: return []
+
+        slow, fast = head, head
+
+        while fast.next and fast.next.next:
+
+            slow = slow.next
+
+            fast = fast.next.next
+
+        
+
+
+        prev, curr = None, slow.next
+
+        while curr:
+
+            nextt = curr.next
+
+            curr.next = prev
+
+            prev = curr
+
+            curr = nextt    
+
+        slow.next = None
+
+        
+
+
+        head1, head2 = head, prev
+
+        while head2:
+
+            nextt = head1.next
+
+            head1.next = head2
+
+            head1 = head2
+
+            head2 = nextt
