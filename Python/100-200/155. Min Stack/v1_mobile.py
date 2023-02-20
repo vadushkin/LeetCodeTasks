9eed105fc276185356c029ccd5ddb6a8ab4e9f8b
Vadim <vadimsalapugin@gmail.com>
2023-02-20 16:00:44 +0500

    155

---
+++ Python/100-200/155. Min Stack/v1_mobile.py
@@ -0,0 +1,32 @@
+class MinStack:
+
+    def init(self):
+        self.__a = []
+
+    def push(self, x: int) -> None:
+
+        m = x
+
+        if self.__a:
+
+            m = self.__a[-1][1]
+
+            if m > x:
+
+                m = x
+
+        self.__a.append((x, m))
+
+    def pop(self) -> None:
+
+        self.__a.pop()
+
+        
+
+    def top(self) -> int:
+
+        return self.__a[-1][0]
+
+    def getMin(self) -> int:
+
+        return self.__a[-1][1]
