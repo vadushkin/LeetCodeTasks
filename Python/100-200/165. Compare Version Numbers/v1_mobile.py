fb05e88e9d104ad4e1f55391d816da0cee87862a
Vadim <vadimsalapugin@gmail.com>
2023-02-20 15:58:25 +0500

    165

---
+++ Python/100-200/165. Compare Version Numbers/v1_mobile.py
@@ -0,0 +1,13 @@
+class Solution:
+    def compareVersion(self, v1: str, v2: str) -> int:
+        v1, v2 = list(map(int, v1.split('.'))), list(map(int, v2.split('.')))  
+
+        for rev1, rev2 in zip_longest(v1, v2, fillvalue=0):
+
+            if rev1 == rev2:
+
+                continue
+
+            return -1 if rev1 < rev2 else 1 
+
+        return 0
