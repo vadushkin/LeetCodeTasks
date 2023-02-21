eaee41d36a5103947df38c89049094eaecee90aa
vaduska <vadimsalapugin@gmail.com>
2023-02-21 14:50:39 +0500

    21

--- Python/1-100/21. Merge Two Sorted Lists/v1.py
+++ Python/1-100/21. Merge Two Sorted Lists/v1.py
@@ -1,13 +1,43 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
-    def mergeTwoLists(self, list1, list2):
-        return sorted(list1 + list2)
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = temp = ListNode(0)
+
+        while list1 is not None and list2 is not None:
+            if list1.val < list2.val:
+                temp.next = list1
+                list1 = list1.next
+            else:
+                temp.next = list2
+                list2 = list2.next
+
+            temp = temp.next
+
+        temp.next = list1 or list2
+
+        return dummy.next
 
 
-s = Solution()
-print(s.mergeTwoLists([1, 2, 4], [1, 3, 4]))
+def main():
+    s = Solution()
+    print(s.mergeTwoLists(ListNode(1, ListNode(2, ListNode(3))), ListNode(2, ListNode(3, ListNode(5)))))
+
+
+if __name__ == '__main__':
+    main()
 
 """Tests:
-1. Quickly.
- 
-2. Quickly.
+1. Runtime 34 ms Beats 89.26% 
+   Memory 13.8 MB Beats 98.61%
+
+2. Runtime 41 ms Beats 52.97% 
+   Memory 13.8 MB Beats 98.61%
 """
